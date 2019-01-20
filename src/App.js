bfde90a16140a0247a441e3bbd4b569c60f21e9e
thirteen <41817587+maguirre13@users.noreply.github.com>
2019-01-20 01:42:27 -0800

    update app.js

--- src/App.js
+++ src/App.js
@@ -24,4 +24,60 @@ const Column = props => {
   return <div className={size}>{props.children}</div>;
 };
 
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+class App extends Component {
+  state = {
+    friends,
+    currentScore: 0,
+    topScore: 0,
+    gameMsg: "",
+    clicked: []
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      gameMsg: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    } else if (newScore === 12) {
+      this.setState({ gameMsg: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      gameMsg: "Game Over!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(friends);
+    this.setState({ friends: shuffledFriends });
+  };
+}
+
 export default App;
